0a055802fc28c8a77ae3fba68d11c3e4c18d5e3f
James Boland <jamespatrickboland@gmail.com>
2023-05-31 16:53:01 +0200

    Adding the (untested) endpoints

--- main.py
+++ main.py
@@ -7,14 +7,31 @@ con = ibis.sqlite.connect("data.db")
 app = FastAPI()
 
 
+@app.get("/")
+def get():
+    return {}
+
+
 @app.post("/{media_type}/")
-async def add(media_type: MediaType, item: Item):
+def add(media_type: MediaType, item: Item):
     table = con.table(media_type.value)
-    table.a
-
-    return {"message": "Hello World"}
+    table.insert(item)
 
 
-@app.get("/{media_type}/{name}")
-async def get(media_type: MediaType, name: str):
-    return {"message": "Hello World"}
+@app.get("/{media_type}/{name}", response_model=Item)
+def get(media_type: MediaType, name: str):
+    table = con.table(media_type.value)
+    res = table.filter(table.name == name)
+    if res.count() > 0:
+        return res[0]
+    else:
+        return {}
+
+
+@app.post("/{media_type}/", response_model=Item)
+def list(media_type: MediaType):
+    table = con.table(media_type.value)
+    if table.count() > 100:
+        return table[:100]
+    else:
+        return table
